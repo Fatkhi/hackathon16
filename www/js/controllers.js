7a56de81bb87213dbb4e3962e50390cb63a08579
Fatkhi <takova.mail.HET@gmail.com>
2016-03-25 15:25:33 +0300

    added search

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope) {
+  })
 
-.controller('ToursCtrl', function($scope, Tours) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  .controller('ToursCtrl', function ($scope, Tours) {
 
-  $scope.tours = Tours.all();
-  $scope.remove = function(tour) {
-    Tours.remove(tour);
-  };
-})
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+    $scope.searchChanged = function () {
+      console.log(this.searchInput);
+    };
+    $scope.tours = Tours.all();
+    $scope.remove = function (tour) {
+      Tours.remove(tour);
+    };
+  })
 
-.controller('TourDetailCtrl', function($scope, $stateParams, Tours) {
-  $scope.chat = Tours.get($stateParams.id);
-})
+  .controller('TourDetailCtrl', function ($scope, $stateParams, Tours) {
+    $scope.chat = Tours.get($stateParams.id);
+  })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
